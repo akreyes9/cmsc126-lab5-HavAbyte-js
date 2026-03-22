9e217fc1f435d92799ddb694f217ee5535180969
amcarreon <atcarreon1@up.edu.ph>
2026-03-22 17:19:45 +0800

    added add_student() function

--- lab5_scripts.js
+++ lab5_scripts.js
@@ -18,3 +18,34 @@ function time_now() {
     document.getElementById('date_time_output').innerHTML =
         `<b>Today is ${dateFormatted}.</b><br><b>The current time is ${timeFormatted}.</b>`;
 }
+
+let students = [];
+
+function add_student() {
+    // INPUTS
+    const name = document.getElementById('name').value.trim();
+    const age = parseInt(document.getElementById('age').value);
+    const email = document.getElementById('email').value;
+    const course = document.getElementById('course').value;
+
+    // GENERATE STUDENT NUMBER
+    const newStudentNum = generateStudentNum();
+    
+    // OBJECT
+    const studentProfile = {
+        studentNum: newStudentNum, 
+        name: name,
+        age: age,
+        up_email: email,
+        course: course
+    };
+
+    students.push(studentProfile);
+    alert('Student profile is successfully submitted.');
+}
+
+// GENERATING 5 RANDOM NUMBERS FOR THE SN
+function generateStudentNum() {
+    const randomNum = Math.floor(10000 + Math.random() * 90000);
+    return '2024${randomNum}';
+}
